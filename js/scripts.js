44557bd45794003552adc690e5d873b6e43625cc
PetrAhtimirov <75142573+PetrAhtimirov@users.noreply.github.com>
2022-12-18 22:13:19 +0300

    scroll animations

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,18 @@
 /* Menu dropdown */
-let menuLink = document.querySelector('.menu-link');
-let menu = document.querySelector('.menu');
-let dropdown = document.querySelector('.dropdown-navigation');
-let dropdownPoints = dropdown.children;
-menuLink.addEventListener('click', function (evt) {
-    evt.preventDefault();
+let showHideNav =  function () {
+    let menu = document.querySelector('.menu');
+    let dropdown = document.querySelector('.dropdown-navigation');
+    let dropdownPoints = dropdown.children;
     menu.classList.toggle("menu-active");
     dropdown.classList.toggle("dropdown-navigation-disabled");
     for (let i = 0; i < dropdownPoints.length; i++) {
         dropdownPoints[i].classList.toggle("animation-points");
     }
-});
-
-/* Preloader */
-let logo = document.querySelector('.logo');
-logo.classList.add('display-none');
-
-let navigation = document.querySelector('.navigation');
-navigation.classList.add('display-none');
-
-let bannerContent = document.querySelector('.banner-text');
-bannerContent.classList.add('display-none');
-
-let cursor = document.querySelector('.cursor');
-cursor.classList.add('display-none');
-
-let circle = document.querySelector('.loader');
-
-let showPage = function () {
-    document.body.classList.add('loaded_hiding');
-    circle.classList.add('zero-opacity');
-    window.setTimeout(function () {
-        document.body.classList.remove('loaded_hiding');
-        document.body.classList.add('loaded');
-    }, 800);
 };
 
+/* Preloader */
 window.onload = function () {
-    logo.classList.remove('display-none');
-    navigation.classList.remove('display-none');
-    bannerContent.classList.remove('display-none');
-    cursor.classList.remove('display-none');
-    setTimeout(showPage, 500);
-    setTimeout(frameLooperDesc, 2700);
-}
-
-window.onbeforeunload = function () {
-    document.body.classList.remove('loaded');
-    window.scrollTo(0, 0);
+    setTimeout(frameLooperDesc, 2000);
 }
 
 /* Typing text description animation */
@@ -76,4 +41,115 @@ for (let smoothLink of smoothLinks) {
             block: "start"
         });
     });
+}
+
+// Animations
+
+const tl = gsap.timeline();
+
+let pageAnimOut = function (container) {
+    return gsap.fromTo(container.querySelector(".preloader"),  {
+        duration: 0.4,
+        top: "-120vh"
+    }, {
+        top: 0
+    }), gsap.to(container.querySelector("main"),  {
+        duration: 0.5,
+        transform: "translateY(200px)"
+    })
+}
+
+let pageAnimIn = function (container) {
+    return gsap.fromTo(container.querySelector(".preloader"),  {
+        top: 0,
+        duration: 0.4
+    }, {
+        top: "100vh",
+        delay: 0.15
+    }), gsap.fromTo(container.querySelector("main"),  {
+        duration: 0.5,
+        transform: "translateY(-200px)"
+    }, {
+        delay: 0.25,
+        transform: "translateY(0)"
+    })
+}
+
+barba.init({
+    transitions: [
+        {
+            name: 'base',
+            async leave(data) {
+                await pageAnimOut(data.current.container);
+                data.current.container.remove();
+            },
+            async enter(data) {
+                await pageAnimIn(data.next.container);
+            }
+        }
+    ],
+    views: [
+        {
+            namespace: 'home',
+            afterEnter() {
+                
+            }
+        }
+    ]
+})
+
+/* Scroll Animations */
+
+const animItems = document.querySelectorAll(".anim-content");
+const animItemsMin = document.querySelectorAll(".anim-title");
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    window.addEventListener('scroll', animOnScrollmin);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 2;
+            
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                //animItem.classList.remove('_active');
+            }
+        }
+    }
+
+    function animOnScrollmin() {
+        for (let index = 0; index < animItemsMin.length; index++) {
+            const animItem = animItemsMin[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 0.4;
+            
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                //animItem.classList.remove('_active');
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
 }
